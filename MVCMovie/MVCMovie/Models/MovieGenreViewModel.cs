c8da03c642b1e850092c6d0a7bed18cb8eadaf1a
ejberry1995 <ber16002@byui.edu>
2020-06-20 18:35:26 -0600

    Add Microsoft MVC tutorial solution

---
+++ MVCMovie/MVCMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public MovieGenreViewModel ()
+        {
+            Genres = new SelectList(new List<string>() { "Action", "Animation", "Comedy", "Drama", "Documentary", "Romance", "Thriller" });
+        }
+
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+        public string Rating { get; set; }
+        public string SortOption { get; set; }
+    }
+}
---
+++ MVCMovie/MVCMovie/Models/SeedData.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCMovie.Data;
+using System;
+using System.Linq;
+
+namespace MVCMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MVCMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Best Two Years",
+                        ReleaseDate = DateTime.Parse("2004-2-20"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "17 Miracles",
+                        ReleaseDate = DateTime.Parse("2011-6-3"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 14.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Saratov Approach",
+                        ReleaseDate = DateTime.Parse("2013-10-9"),
+                        Genre = "Thriller",
+                        Rating = "PG-13",
+                        Price = 14.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Meet the Mormons",
+                        ReleaseDate = DateTime.Parse("2015-2-26"),
+                        Genre = "Documentary",
+                        Rating = "PG",
+                        Price = 2.50M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Testaments: Of One Fold and One Sheperd",
+                        ReleaseDate = DateTime.Parse("2000-3-24"),
+                        Genre = "Drama",
+                        Rating = "Unrated",
+                        Price = 1.13M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
