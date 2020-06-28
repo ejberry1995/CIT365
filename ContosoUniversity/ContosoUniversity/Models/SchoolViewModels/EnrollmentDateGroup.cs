48cbe363f9c3bfe2e053195dfc0ee5a2d3e242b6
ejberry1995 <ber16002@byui.edu>
2020-06-27 21:41:27 -0600

    Completed Contoso University MVC tutorial

---
+++ ContosoUniversity/ContosoUniversity/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime? EnrollmentDate { get; set; }
+
+        [Display(Name = "Student Count")]
+        public int StudentCount { get; set; }
+    }
+}
